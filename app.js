f50252eba870bad44ff08226a9a905c408056427
Talal <talalahmed252@gmail.com>
2021-08-23 16:09:04 +0500

    OOP Start

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
-let  favMovie = (movie = "The Room", name = "Tommy Wiseau") => console.log(`My name is ${name} and my favourite movie is ${movie}`);
+const MAX = 800;
+let container = document.getElementById('sqCont');
+class Square {
+    constructor(x, y) {
+        this.div = document.createElement('div');
+        this.div.classList.add('square');
+        this.div.style.left = `${x}px`;
+        this.div.style.top = `${y}px`;
+        this.div.addEventListener('click', () => {
+            this.updateColor();
+            this.updateLocation();
+        })
+        this.updateColor();
+        container.appendChild(this.div);
+    }
 
-favMovie();
-favMovie("Harry Potter", "Talal Ahmed");
+    updateLocation() {
+        let x = randomVal(0, MAX);
+        let y = randomVal(0, MAX);
+        this.div.style.left = `${x}px`;
+        this.div.style.top = `${y}px`;
+    }
 
+    updateColor() {
+        let randomColor = `rgb(${randomVal(0, 255)}, ${randomVal(0, 255)}, ${randomVal(0, 255)})`;
+        this.div.style.backgroundColor = randomColor;
+    }
 
-let getFirstName = (name) => {
-    name = name.split(' ');
-    return name[0];
-};
-
-let getFirstNameConsise = name => name.split(' ')[0];
-
-console.log(getFirstName("Talal Ahmed"));
-console.log(getFirstNameConsise("Talal Ahemd"));
-
-function itis(name, location, favFood) {
-    console.log(`${name}, ${location}, ${favFood}`);
 }
 
-let arr = ["Talal", "Islamabad", "MithiMithi"];
-itis(...arr);
+let sqButton = document.getElementById('sqBtn');
+sqButton.addEventListener('click', InsertSquare);
+function InsertSquare() {
+    let x = randomVal(0, MAX);
+    let y = randomVal(0, MAX);
+    let sq = new Square(x, y);
+}
+
+function randomVal(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
